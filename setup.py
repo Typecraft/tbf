dcf5c36563f00d006eb648dcd72fd10085647f23
Tormod Haugland <tormod.haugland@gmail.com>
2018-02-02 11:30:46 +0100

    Bump version: 0.1.0 → 0.2.0

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ test_requirements = [
 
 setup(
     name='tbf',
-    version='0.1.0',
+    version='0.2.0',
     description="Typecraft Binary Format, a super-optimized IGT format",
     long_description=readme + '\n\n' + history,
     author="Tormod Haugland",
--- tbf/__init__.py
+++ tbf/__init__.py
@@ -4,4 +4,4 @@
 
 __author__ = """Tormod Haugland"""
 __email__ = 'tormod.haugland@gmail.com'
-__version__ = '0.1.0'
+__version__ = '0.2.0'
